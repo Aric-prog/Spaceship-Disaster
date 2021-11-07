32065610cb4d81ba74268481a4c289dfad7f30c3
dankpanda <yowenyo17@gmail.com>
2021-11-07 20:22:12 +0700

    adding kambing

--- app/controllers/client.js
+++ app/controllers/client.js
@@ -5,16 +5,10 @@ router.get('/testing', function(req, res){
     res.sendFile(path.join(__dirname, '../client', 'test_socket.html'));
 });
 router.get('/', function(req, res){
-<<<<<<< HEAD
     res.sendFile(path.join(__dirname, '../client', 'main_menu.html'));
 });
-
-=======
-    res.sendFile(path.join(__dirname, '../client', 'test_babylon.html'));
-})
 router.get('/kambing', function(req, res){
     res.sendFile(path.join(__dirname, '../client', 'learning.html'));
-})
->>>>>>> 7e7b6d98ec59c5b550e45989e13017515aa814f8
+});
 
 module.exports = router;
